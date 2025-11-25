0112ac589f8ad8f7746fdba63993f7cd37aecad5
Vick <vike11107@gmail.com>
2025-11-25 14:33:12 +0800

    fix api routing

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -9,103 +9,105 @@ require_relative 'helpers'
 module LingoBeats
   # Web App
   class App < Roda
-    plugin :flash
     plugin :all_verbs # allows HTTP verbs beyond GET/POST (e.g., DELETE)
-    plugin :public, root: 'app/presentation/public'
+    # plugin :public, root: 'app/presentation/public'
     plugin :halt
     plugin :multi_route
 
     route do |routing|
-      routing.public # serve /public files
+      # routing.public # serve /public files
       response['Content-Type'] = 'application/json'
 
       # GET /
       routing.root do # Roda routing.root is for GET /
         @current_page = :home
 
+        routing.halt(200, { status: 'ok', message: 'API is working' }.to_json)
+
         # Show popular songs on home page
-        result = Service::ListSongs.new.call(:popular)
-        RouteHelpers::Response.call(routing, result, Representer::SongsList)
+        # result = Service::ListSongs.new.call(:popular)
+        # RouteHelpers::Response.call(routing, result, Representer::SongsList)
       end
 
-      # 子路由
-      routing.multi_route
-    end
+      
 
-    # /tutorial
-    route('tutorial') do |routing|
-      routing.get do
-        @current_page = :tutorial
-        view 'tutorial'
-      end
-    end
-
-    # /history
-    route('history') do |routing|
-      routing.get do
-        @current_page = :history
-        view 'history'
-      end
-    end
-
-    route('api/v1') do |routing|
-      # song-related routes
-      routing.on 'songs' do
-        # GET /songs?category=...&query=...
-        routing.is do
-          routing.get do
-            list_req = Request::SongList.new(routing.params)
-            result = Service::ListSongs.new.call(list_request: list_req)
-
-            RouteHelpers::Response.call(routing, result, Representer::SongsList)
-          end
+      # /tutorial
+      routing.on 'tutorial' do
+        routing.get do
+          @current_page = :tutorial
+          view 'tutorial'
         end
+      end
 
-        # /:id
-        routing.on String do |song_id|
-          # GET /songs/:id
+      # /history
+      routing.on 'history' do
+        routing.get do
+          @current_page = :history
+          view 'history'
+        end
+      end
+
+      routing.on 'api/v1' do
+        # song-related routes
+        routing.on 'songs' do
+          # GET /songs?category=...&query=...
           routing.is do
             routing.get do
-              result = Service::AddSong.new.call(song_id:)
+              list_req = Request::SongList.new(routing.params)
+              result = Service::ListSongs.new.call(list_request: list_req)
 
-              RouteHelpers::Response.call(routing, result, Representer::Song)
+              RouteHelpers::Response.call(routing, result, Representer::SongsList)
             end
           end
 
-          # GET /songs/:id/lyrics
-          routing.on 'lyrics' do
-            routing.get do
-              result = Service::AddLyric.new.call(song_id:)
+          # /:id
+          routing.on String do |song_id|
+            # GET /songs/:id
+            routing.is do
+              routing.get do
+                result = Service::AddSong.new.call(song_id:)
 
-              RouteHelpers::Response.call(routing, result, Representer::Lyric)
+                RouteHelpers::Response.call(routing, result, Representer::Song)
+              end
             end
-          end
 
-          # GET /songs/:id/level
-          routing.on 'level' do
-            routing.get do
-              result = Service::AnalyzeSongLevel.new.call(song_id:)
+            # GET /songs/:id/lyrics
+            routing.on 'lyrics' do
+              routing.get do
+                result = Service::AddLyric.new.call(song_id:)
 
-              RouteHelpers::Response.call(routing, result, Representer::SongLevel)
+                RouteHelpers::Response.call(routing, result, Representer::Lyric)
+              end
             end
-          end
 
-          # POST /songs/:id/materials
-          routing.on 'materials' do
-            routing.post do
-              result = Service::AddMaterial.new.call(song_id:)
+            # GET /songs/:id/level
+            routing.on 'level' do
+              routing.get do
+                result = Service::AnalyzeSongLevel.new.call(song_id:)
 
-              RouteHelpers::Response.call(routing, result, Representer::Material)
+                RouteHelpers::Response.call(routing, result, Representer::SongLevel)
+              end
             end
-            # GET /songs/:id/materials
-            routing.get do
-              result = Service::GetMaterials.new.call(song_id:)
 
-              RouteHelpers::Response.call(routing, result, Representer::Material)
+            # POST /songs/:id/materials
+            routing.on 'materials' do
+              routing.post do
+                result = Service::AddMaterial.new.call(song_id:)
+
+                RouteHelpers::Response.call(routing, result, Representer::Material)
+              end
+              # GET /songs/:id/materials
+              routing.get do
+                result = Service::GetMaterials.new.call(song_id:)
+
+                RouteHelpers::Response.call(routing, result, Representer::Material)
+              end
             end
           end
         end
       end
+      # 子路由
+      routing.multi_route
     end
   end
 end
